00cae8fb1df4b07bf9cd955aad364eb32694f236
rico <kingandrei6405@gmail.com>
2026-02-19 01:31:09 -0800

    Answer

--- Midterm_API/Controllers/StudentController.cs
+++ Midterm_API/Controllers/StudentController.cs
@@ -10,7 +10,7 @@ namespace Midterm_API.Controllers
     {
         private readonly StudentService _studentService;
 
-        public StudentController(StudentService studentService)
+        public StudentController(StudentService studentService) 
         {
             _studentService = studentService;
         }
@@ -18,7 +18,42 @@ namespace Midterm_API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_studentService.GetAllStudents());
+            return Ok(_studentService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var student = _studentService.GetStudent(id);
+            return student == null ? NotFound() : Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            _studentService.AddStudent(student);
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Student student)
+        {
+            student.Id = id;
+            _studentService.UpdateStudent(student);
+            return student == null ? NotFound() : Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var studentExist = _studentService.GetStudent(id);
+            if (studentExist == null)
+            {
+                return NotFound();
+            }
+            _studentService.DeleteStudent(id);
+            return Ok();
         }
 
         //GET
@@ -41,6 +76,6 @@ namespace Midterm_API.Controllers
         //DELETE
         //Create DeleteStudent endpoint here
         //Accept "id" as parameter
-       
+
     }
 }
--- Midterm_API/Services/StudentService.cs
+++ Midterm_API/Services/StudentService.cs
@@ -1,38 +1,29 @@
-﻿using Midterm_API.DAL;
+﻿using Microsoft.AspNetCore.Mvc;
+using Midterm_API.DAL;
 using Midterm_API.Entities;
 
 namespace Midterm_API.Services
 {
-    public class StudentService
-    {
-        private readonly StudentRepository _repository;
+            public class StudentService
+            {
+                private readonly StudentRepository _repository;
 
-        public StudentService(StudentRepository repository)
-        {
-            _repository = repository;
+                public StudentService(StudentRepository repository)
+                {
+                    _repository = repository;
+                }
+
+                public IEnumerable<Student> GetStudents()
+                {
+                    return _repository.GetAll().OrderByDescending(p => p.Id);
+                }
+
+                public Student GetStudent(int id) => _repository.GetById(id);
+
+                public void AddStudent(Student student) => _repository.Add(student);
+
+                public void UpdateStudent(Student student) => _repository.Update(student);
+
+                public void DeleteStudent(int id) => _repository.Delete(id);
+            }
         }
-
-        public IEnumerable<Student> GetAllStudents()
-        {
-            return _repository.GetAll().OrderByDescending(p => p.Id);
-        }
-
-        //Create GetSingleStudent method here
-        //Accept "id" as parameter and return Student
-
-
-
-        //Create AddStudent method here
-        //Accept Student object as parameter and return void
-
-
-
-        //Create UpdateStudent method here
-        //Accept Student object as parameter and return void
-
-
-
-        //Create DeleteStudent method here
-        //Accept "id" as parameter and return void
-    }
-}
