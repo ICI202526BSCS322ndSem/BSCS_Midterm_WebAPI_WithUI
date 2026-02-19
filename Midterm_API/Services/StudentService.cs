9a822169de7e47a59811eac21fcb3bcd194c50a9
Saeko07 <shaungatchalian772@gmail.com>
2026-02-19 09:53:28 +0800

    midterm

--- Midterm_API/Services/StudentService.cs
+++ Midterm_API/Services/StudentService.cs
@@ -1,4 +1,5 @@
-﻿using Midterm_API.DAL;
+﻿using Microsoft.AspNetCore.Mvc;
+using Midterm_API.DAL;
 using Midterm_API.Entities;
 
 namespace Midterm_API.Services
@@ -19,20 +20,20 @@ namespace Midterm_API.Services
 
         //Create GetSingleStudent method here
         //Accept "id" as parameter and return Student
-
+        public Student GetSingleStudent(int id) => _repository.GetById(id);
 
 
         //Create AddStudent method here
         //Accept Student object as parameter and return void
-
-
+        public void AddStudent (Student student) =>_repository.Add(student);
 
         //Create UpdateStudent method here
         //Accept Student object as parameter and return void
-
-
+        public void UpdateStudent(Student student) =>_repository.Update(student);
 
         //Create DeleteStudent method here
         //Accept "id" as parameter and return void
+
+        public void DeleteStudent(int id) =>_repository.Delete(id);
     }
 }
